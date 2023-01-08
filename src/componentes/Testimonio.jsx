dee64787db31b8b4d6a3a6fdb28296ce37f492c5
Nazareno <nazareno.jobs@gmail.com>
2023-01-08 04:12:01 -0300

    uso de props por primera vez

--- src/componentes/Testimonio.jsx
+++ src/componentes/Testimonio.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 import '../hojas-de-estilos/Testimonio.css';
 
-function Testimonio() {
+function Testimonio(props) {
   return (
     <div className='contenedor-testimonio' >
       <img 
         className='imagen-testimonio' 
-        src={require('../imagenes/emma.png')}
+        src={require(`../imagenes/testimonio-${props.imagen}.png`)}
         alt= 'Perfil de Emma'
       />
       <div className='contenedor-texto-testimonio'>
-        <p className='nombre-testimonio'>Emma Bostian en Suecia</p>
-        <p className='cargo-testimonio'>Ingeniera de Software en Spotify</p>
-        <p className='texto-testimonio'>"Siempre he tenido problemas para aprender JavaScript. Tomé muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como un ingeniero de software en Spotify".</p>
+        <p className='nombre-testimonio'>{props.nombre} en {props.pais}</p>
+        <p className='cargo-testimonio'>{props.cargo} en {props.empresa}</p>
+        <p className='texto-testimonio'>"{props.testimonio}"</p>
       </div>
     </div>
-  );
+  ); 
 }
 
 export default Testimonio;
